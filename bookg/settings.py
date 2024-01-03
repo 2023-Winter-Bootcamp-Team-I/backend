ace676887c255cf98f7fb5b0774c6a587893e490
KimJinYong <kjy154969@gmail.com>
2024-01-03 16:02:24 +0900

    도커랑 장고 진짜 연동 되었음. db까지

--- bookg/settings.py
+++ bookg/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bookg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306'
     }
 }
 
