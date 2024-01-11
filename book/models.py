c28ab2b6188f7b3ced764148611672ec15c5aa63
SeongHun.Choi <noah071333@gmail.com>
2024-01-11 15:13:51 +0900

    feat: 로그인 회원가입 기능 구현

--- book/models.py
+++ book/models.py
@@ -14,10 +14,3 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
     is_deleted = models.DateTimeField(default=None, null=True)
-
-    def update_date(self):
-        self.updated_at = timezone.now()
-        self.save()
-
-    class Meta:
-        db_table = 'book'
--- user/models.py
+++ user/models.py
@@ -55,11 +55,4 @@ class User(models.Model):
     name = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
-    is_deleted = models.BooleanField(default=False)
-
-    def update_date(self):
-        self.updated_at = timezone.now()
-        self.save()
-
-    class Meta:
-        db_table = 'user'
+    is_deleted = models.BooleanField(default=False)  # 확인바랍니다
