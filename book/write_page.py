a0d04990c03d43ccb04f4a84981857b24883d45e
KimJinYong <kjy154969@gmail.com>
2024-01-12 21:31:08 +0900

    글 생성,  DALL-E, DB 부분 함수 부분 미완성, receive 함수에서 이미지 셀러리 아직 X

--- book/write_page.py
+++ book/write_page.py
@@ -1,6 +1,9 @@
 import json
+
+import openai
 from channels.generic.websocket import WebsocketConsumer
-import page
+from page.models import Page
+
 
 class WritePage(WebsocketConsumer):
 
@@ -16,13 +19,13 @@ class WritePage(WebsocketConsumer):
     # 소켓 통신 연결 해제
     def disconnect(self, closed_code):
         # 만약에 중간에 끊킨 경우, book_id와 관련된 것 전부 삭제
-        book_object = page.objects.get(id=self.book_id)
-        pages = page.objects.filter(book_id=book_object)
+        book_object = Page.objects.get(id=self.book_id)
+        pages = Page.objects.filter(book_id=book_object)
         page_num = pages.count()
 
         # 가져온 페이지의 수와 예상 페이지 수가 다르면 삭제
         if page_num != self.page_num:
-            page.objects.filter(book_id=self.book_id).delete()
+            Page.objects.filter(book_id=self.book_id).delete()
 
         for pages in pages:
             try:
@@ -31,38 +34,137 @@ class WritePage(WebsocketConsumer):
                 en_content = pages.en_content
                 print(ko_content, en_content)
             except:
-                page.objects.filter(book_id=self.book_id).delete()
+                Page.objects.filter(book_id=self.book_id).delete()
         pass
 
     # 소켓 통신 (메세지)
     def receive(self, text_data):
+
         text_data_json = json.loads(text_data) #data를 받음
+        page_cnt = text_data_json.get('page_cnt')
+
+        if text_data_json['type'] == 'start':
+            user_info = self.extract_user_info(text_data_json)
+            responses = self.generate_start_gpt_responses(user_info) # 시작
+            # 페이지 번호 증가 및 응답 반환
+            page_cnt += 1
+            self.send_response_to_client(responses, page_cnt)
+
+        elif text_data_json['type'] == 'ing':
+            # 책 내용 가져온거 처리하기
+            choice = text_data_json.get('choice')
+
+            ko_content = text_data_json.get('ko_content')
+            en_content = text_data_json.get('en_content')
+
+            self.save_story_to_db(page_cnt, ko_content, en_content)
+
+            image_uuid = self.generate_dalle_image(en_content) # 비동기 함수 ??
+
+            # 6번째 페이지 처리
+            if page_cnt == 6:
+                responses = self.generate_end_gpt_responses(choice) # 끝
+                page_cnt += 1
+                self.send_response_to_client(responses, page_cnt)
+
+            else: # 2~5번 페이지 처리
+                responses = self.generate_ing_gpt_responses(choice) # 중간
+                page_cnt += 1
+                self.send_response_to_client(responses, page_cnt)
+
+        elif text_data_json['type'] == 'end': # 마지막 선택 처리
+
+            ko_content = text_data_json.get('ko_content')
+            en_content = text_data_json.get('en_content')
+
+            self.save_story_to_db(page_cnt, ko_content, en_content)
+
+            image_uuid = self.generate_dalle_image(en_content)
+
+######################## 함수들 ########################
+    def generate_start_gpt_responses(self, user_info):
+        # 지피티씨 호출해서 만들고 반환하기. -> 이 정보들로 이야기 만들어줘 두가지로 6번만 선택할거야 어쩌구 저쩌구
+        self.conversation = [
+            {
+                "role": "system",
+                "content": "당신은 어린이 동화 작가입니다. 어떤 리액션도 하지마세요. 사용자가 원하는 것에 대해 양식에 맞춰서 글만 써주세요."
+            },
+            {
+                "role": "user",
+                "content": f"{user_info.username} {user_info.gender} {user_info.age} {user_info.lan} {user_info.fairytale} 이 요소들로 동화 써줘~"
+            }
+        ]
+
+    def generate_ing_gpt_responses(self, choice):
+        # 지피티씨 호출해서 만들고 반환하기. -> 내가 선택한 이야기로 진행해주고 계속 이어서 두가지로 해줘
+        self.conversation = [
+            {
+                "role": "system",
+                "content": choice + '이 요소로 프롬프트 생성'
+            },
+        ]
+
+    def generate_end_gpt_responses(self, choice):
+        # 지피티씨 호출해서 만들고 반환하기. -> 내가 선택한 이야기로 이야기 마무리 엔딩 내줘
+        self.conversation = [
+            {
+                "role": "system",
+                "content": choice + '이 요소로 프롬프트 생성하는데 이제 여기서 고른 문장으로 마무리 엔딩 내달라고 하기'
+            },
+        ]
+
+
+    def extract_user_info(self, data):
+        user_info = {
+            'username': data.get('username'),
+            'gender': data.get('gender'),
+            'age': data.get('age'),
+            'lan': data.get('lan'),
+            'fairytale': data.get('fairytale')
+        }
+        return user_info
+
+
+    def save_story_to_db(self, page_cnt, ko_content, en_content):
+        Page.objects.create(page_cnt=page_cnt, ko_content=ko_content, en_content=en_content)
+
+
+    def generate_dalle_image(self, en_content):
+        print("달리야 해줘")
+        # 달리 호출해서 만들고 반환 -> 이 내용에 적절한 그림을 그려줘
+        # save_story_to_db 함수랑 합쳐야 하는 것 같은데 맞나 en_content 이용해서 image_url 받으면 db에 넣으면 되자나
+
+
+    # 응답을 클라이언트한테 전송함.
+    def send_response_to_client(self, responses, page_cnt):
+        # GPT-3 스트리밍 API 호출
+        for response in openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages= responses,
+            temperature=0.5,
+            stream=True
+        ):
+            # 각 응답 조각 처리
+            message = response.choices[0].message['content']
+            if message:
+                #클라이언트에게 실시간으로 메세지 전송
+                self.send(text_data=json.dumps({"message": message}))
+
+
         # if 초기 생성 -> 초기 값을 서버가 받음 (n번 페이지) = 0 <start>
-
             # data json 형태니까 나눌 수 있겠지
-
             # 나이 성별 이름 동화 등을 가지고 gpt 함수를 부를거야
-
             # 답변이 생성되면 프론트로 디비 저장 않고, 다시 보내줘 -> 두개 다 반환 n++ 까지 하고 n 전달
-
-
         # elif 책 만들고 있는 경우 -> 선택한 스토리를 서버가 받음 (n번 페이지) <ing>
-
             # data json 형태니까 나눌 수 있겠지
-
             # 1. 받은 스토리(n번 페이지)를 db에 저장 -> 페이지 숫자랑 내용이랑 등등 + 이미지 UUID
-
             # 2. 받은 스토리(n번 페이지) 셀러리로 넘겨서 달리로 그림 생성 하기 + 위 이미지 UUiD
-
             # 3. if n+1번 페이지 제작 중 == 6 <- 분기 이유 : 프롬프팅 함수가 달라짐.
                 # 3-1. 이전에 받은 값(n) 받아서 이야기 2개 생성
                 # 3-2. 답변 생성되면 프론트로 다시 보내기 -> 두개다 반환 (영어버전 한글 버전 다) n++
-
             # 3. elif 마지막이 아니라면(2-5번 페이지 제작중)
                 # 3-1. 이전에 받은 값(n) 받아서 이야기 2개 생성
                 # 3-2. 답변 생성되면 프론트로 다시 보내기 -> 두개다 반환 (영어버전 한글 버전 다) n++
-
-
         # else 사용자의 마지막 선택 end / n이 7일때
-            #1. 받은 스토리(6번 페이지)를 db에 저장 -> 페이지 숫자랑 내용이랑 등등 + 이미지 UUID
-            #2. 받은 스토리(6번 페이지) 셀러리로 넘겨서 달리로 그림 생성 하기 + 위 이미지 UUiD
+            # 1. 받은 스토리(6번 페이지)를 db에 저장 -> 페이지 숫자랑 내용이랑 등등 + 이미지 UUID
+            # 2. 받은 스토리(6번 페이지) 셀러리로 넘겨서 달리로 그림 생성 하기 + 위 이미지 UUiD
