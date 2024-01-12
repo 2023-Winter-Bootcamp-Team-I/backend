14697c64fcad7f0caec3a21eb5ab235ef701abf7
SeongHun.Choi <noah071333@gmail.com>
2024-01-13 03:28:32 +0900

    feat: 동화책 제목 작성 API 구현

--- book/serializers.py
+++ book/serializers.py
@@ -1,24 +1,33 @@
 from rest_framework import serializers
-
 from book.models import Book
 from page.models import Page
+
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['user_id', 'username', 'fairytale', 'gender', 'age']
 
+
 class BookCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['book_id', 'username', 'fairytale', 'gender', 'age']
+
 
 class ContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Page
         fields = ['book_id', 'page_num', 'ko_content', 'en_content']
 
+
 class ContentChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Page
         fields = ['page_id', 'book_id', 'page_num', 'ko_content', 'en_content']
+
+
+class TitleCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['book_id', 'title']
