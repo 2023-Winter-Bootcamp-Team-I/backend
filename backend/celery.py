8ff005f14ebb18b6b729e27dc8ecd3dba2d6e53b
KimJinYong <kjy154969@gmail.com>
2024-01-19 02:22:24 +0900

    일단 도커에 올려서 연동은 되는 것 같음... 이제 tasks.py 파일을 손보면 됨

--- backend/celery.py
+++ backend/celery.py
@@ -9,11 +9,13 @@ from celery.schedules import crontab
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 app = Celery('backend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+#등록된 장고 앱 설정에서 task 불러오기
+app.autodiscover_tasks()
+
 app.conf.beat_schedule = {
     'printTime': { # 스케줄링 이름
         'task': 'book.tasks.printTime',
         'schedule': crontab(), # 인자 없으면 매 분마다 실행
     }
 }
-#등록된 장고 앱 설정에서 task 불러오기
-app.autodiscover_tasks()
