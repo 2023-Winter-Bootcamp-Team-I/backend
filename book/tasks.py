a99ae04cbeb4ac4cf18f6a0f905d8a8805b77861
KimJinYong <kjy154969@gmail.com>
2024-01-29 16:43:14 +0900

    잘됨

--- book/tasks.py
+++ book/tasks.py
@@ -22,7 +22,8 @@ def generate_dalle_image_async(image_uuid, enContent):
             model="dall-e-3",
             prompt=f"{enContent}"
                    f" Please draw the sentence in a cute art 2d style."
-                   f"Please draw only in pictures.",
+                   f"Bright color tones, cartoonish characters, clear outlines."
+                   f"Please make sure there is no text in the picture.",
             n=1,
             size="1024x1024"
         )
